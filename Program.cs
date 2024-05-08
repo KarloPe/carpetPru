358d72e46e454fa8dbe0fe187db0cbd536f14835
Karloo <karlontxo@gmail.com>
2024-05-07 21:33:56 -0300

    version 2 due

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace libb{
+    class Miiaa{
+        static void Main(){
+            Console.WriteLine ("holaaaa");
+
+        }
+    }
+}
